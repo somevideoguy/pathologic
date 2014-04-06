0fe70e1812723b3689cf57811c865aee900afa43
Mihai Ciumeica <cmihai@gmail.com>
2014-04-06 22:50:08 +0200

    Factored out some common functions into pathologic.py

--- decode_keys.py
+++ decode_keys.py
@@ -1,13 +1,6 @@
-import sys, struct, codecs, argparse
+import argparse
 
-"""
-    Format of the main.dat string file (all numbers are little-endian):
-
-    +--------------------------------+-------------------------------+--------------------+------------------   ...   --------------+----------------------+------- ...
-    |  Number of strings (4 bytes)   |          ID (4 bytes)         | Len (1 or 2) bytes | First string (UCS-2) little-endian      |  Len (1 or 2) bytes  |  Second string ...
-    +--------------------------------+-------------------------------+--------------------+------------------   ...   --------------+----------------------+------- ...
-
-"""
+from pathologic import *
 
 def parse_args():    
     parser = argparse.ArgumentParser(description='Extracts the data from the Pathologic string file (Strings/main.dat).')
@@ -17,42 +10,6 @@ def parse_args():
 
     return parser.parse_args()
 
-def read_bytes(f, count):
-    return f.read(count)
-
-def read_int(f):
-    b = f.read(4)
-    return struct.unpack('<i', b)[0]
-
-def decode_file(infile):
-    strings = []
-
-    with open(infile, 'rb') as f:
-        count = read_int(f)
-        for i in xrange(count):
-            id = read_int(f)
-            byte1 = ord(f.read(1))
-            if byte1 & 0x80:
-                byte2 = ord(f.read(1))
-                len = (byte1 & 0x7f) + (byte2 << 7)
-            else:
-                len = byte1
-                
-            strings.append((id, f.read(len * 2).decode("utf-16")))
-
-    strings.sort(key=lambda str: str[0])
-
-    return strings
-
-def write_xml(strings, outfile):
-    with codecs.open(outfile, "wb") as g:
-        g.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<strings>\r\n".encode("utf-8"))
-
-        for (id, string) in strings:
-            g.write((u"<string id=\"%s\"><![CDATA[%s]]></string>\r\n" % (id, string)).encode("utf-8"))
-        g.write("</strings>\r\n".encode("utf-8"))
-
-
 if __name__ == '__main__':
     args = parse_args()
 
@@ -60,5 +17,5 @@ if __name__ == '__main__':
     outfile = args.o or (infile + '.xml')
     #lang = args.lang
 
-    strings = decode_file(infile)
-    write_xml(strings, outfile)
+    strings = read_maindat(infile)
+    write_maindat_xml(strings, outfile)
--- encode_keys.py
+++ encode_keys.py
@@ -1,40 +1,18 @@
-import sys, argparse, xml.dom.minidom, codecs, struct
+import argparse
+
+from pathologic import *
 
 def parse_args():
-	parser = argparse.ArgumentParser(description="Recreates the Pathologic strings file from an XML input.")
-	parser.add_argument('-i', '--infile', required=True)
-	parser.add_argument('-o', '--outfile', required=True)
+    parser = argparse.ArgumentParser(description="Recreates the Pathologic strings file from an XML input.")
+    parser.add_argument('-i', '--infile', required=True)
+    parser.add_argument('-o', '--outfile', required=True)
 
-	return parser.parse_args()
-
-def write_int(g, n):
-	g.write(struct.pack('<I', n))
-
-def write_str(g, str):
-	g.write(str.encode('utf-16-le'))
-
-def write_str_len(g, len):
-	if len < 0x80:
-		g.write(struct.pack('B', len))
-	else:
-		g.write(struct.pack('B', (len & 0x7f) + 0x80))
-		g.write(struct.pack('B', (len >> 7)))
+    return parser.parse_args()
 
 if __name__ == '__main__':
-	args = parse_args()
-
-	dom = xml.dom.minidom.parse(args.infile)
-
-	with open(args.outfile, 'wb') as out:
-		strings = dom.getElementsByTagName('string');
-
-		write_int(out, len(strings))
-		for str in strings:
-			write_int(out, int(str.getAttribute('id')))
-
-			if str.firstChild is None:
-				write_str_len(out, 0)
-			else:
-				data = str.firstChild.data
-				write_str_len(out, len(data))
-				write_str(out, data)
+    args = parse_args()
+    
+    with open(args.outfile, 'wb') as out:
+        with open(args.infile, 'r') as inf:        
+            strings = read_maindat_xml(inf)            
+            write_maindat(strings, out)
